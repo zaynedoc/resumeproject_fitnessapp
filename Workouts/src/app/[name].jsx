95e4b9bc55ff7deefd2fecd49c82c61cea0160d6
Zayne <49252407+zaynedoc@users.noreply.github.com>
2025-04-27 06:39:21 +0000

    Expand/compress details function

--- Workouts/src/app/[name].jsx
+++ Workouts/src/app/[name].jsx
@@ -2,9 +2,12 @@ import { View, Text, StyleSheet, ScrollView} from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 import exercises from '../../assets/data/exercises.json';
 import { Stack } from 'expo-router';
+import {useState } from 'react';
 
 export default function ExerciseDetailsScreen() {
     const params = useLocalSearchParams();
+
+    const [isInstructionExpanded, setIsInstructionExpanded] = useState(false);
 
     const exercise = exercises.find(item => item.name === params.name);
 
@@ -28,8 +31,12 @@ export default function ExerciseDetailsScreen() {
             </View>
 
             <View style ={styles.panel}>
-                <Text style = {styles.instructions}>
+                <Text style = {styles.instructions} numberOfLines = {isInstructionExpanded ? 0 : 3}>
                     {exercise.instructions}
+                </Text>
+
+                <Text onPress={() => setIsInstructionExpanded(!isInstructionExpanded)} style={styles.seeMore}>
+                    {isInstructionExpanded ? 'See less' : 'See more'}
                 </Text>
             </View>
         </ScrollView>
@@ -76,5 +83,11 @@ const styles = StyleSheet.create({
             fontSize: 15,
             lineHeight: 25,
         },
+        seeMore: {
+            alignSelf: 'center',
+            padding: 10,
+            fontWeight: 'bold',
+            color: 'grey',
+        }
     }
 );
